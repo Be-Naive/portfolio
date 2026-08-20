631f9578db73d490273402476d0423cfdf1ebca6
zm wang <wzm3456789@gmail.com>
2026-08-20 00:02:59 -0400

    Allow configurable dashboard port

--- run.py
+++ run.py
@@ -1,12 +1,15 @@
+import os
 from wsgiref.simple_server import make_server
 
 from portfolio_app.server import PortfolioApplication
 
 
 def main():
+    host = os.getenv("HOST", "127.0.0.1")
+    port = int(os.getenv("PORT", "8000"))
     app = PortfolioApplication()
-    with make_server("127.0.0.1", 8000, app) as server:
-        print("Serving portfolio dashboard at http://127.0.0.1:8000")
+    with make_server(host, port, app) as server:
+        print(f"Serving portfolio dashboard at http://{host}:{port}")
         server.serve_forever()
 
 
